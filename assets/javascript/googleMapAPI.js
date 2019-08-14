ee32a41fdde9367ed88b870da96275de99c50dd2
Rita Manley <manley.rita@gmail.com>
2019-08-14 11:05:18 -0700

    map defaults to Puget Sound, CSS reset loaded

--- assets/javascript/googleMapAPI.js
+++ assets/javascript/googleMapAPI.js
@@ -10,7 +10,7 @@
       var day;
       function initAutocomplete() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -33.8688, lng: 151.2195},
+          center: {lat: 48.4897, lng: -122.6756},
           zoom: 13,
           mapTypeId: 'roadmap'
         });
