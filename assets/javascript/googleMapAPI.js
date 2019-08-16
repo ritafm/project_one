598be235f0acfb49ba92c05fc79f1dd75e5f70da
Rita Manley <manley.rita@gmail.com>
2019-08-15 21:50:08 -0700

    fixed style.  Almost there!

--- assets/javascript/googleMapAPI.js
+++ assets/javascript/googleMapAPI.js
@@ -28,7 +28,7 @@ function initMap() {
     // Create the search box and link it to the UI element.
     var input = document.getElementById('pac-input');
     var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
     // Bias the SearchBox results towards current map's viewport.
     map.addListener('bounds_changed', function () {
         searchBox.setBounds(map.getBounds());
@@ -160,8 +160,6 @@ function initMap() {
                         // increment our counter so we get unique IDs for each day
                         dayNum += 1;
                     }
-\
-
                 });
 
 
